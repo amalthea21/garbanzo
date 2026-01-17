c497ac19ffdea3b8348810ae90b71a29bacb1c5f
amalthea <oliver.hitz.7@gmail.com>
2026-01-17 19:34:21 +0100

    fixes

--- include/TaskPooler.h
+++ include/TaskPooler.h
@@ -13,15 +13,8 @@ private:
     std::mutex amountMutex;
 
 public:
-    // Add files to the task pool
     void addTaskPool(std::vector<File> files);
-
-    // Get total findings across all pooled tasks using threads
     int getTotalFindings(Arguments arguments, const std::string& search);
-
-    // Clear all pooled tasks
     void clear();
-
-    // Get the number of task pools
     size_t getPoolCount() const;
 };
